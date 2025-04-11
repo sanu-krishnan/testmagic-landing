b5cbda13d5b0fa47bea45f1090280405e30144fa
Sanu <sanukrishnansb@gmail.com>
2025-04-11 18:39:34 +0530

    Tailwind configuration added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import vue from '@astrojs/vue';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import vue from "@astrojs/vue";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  integrations: [vue(), tailwind()],
 });
