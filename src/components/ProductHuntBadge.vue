76fbdd6f61ff6d4b065468abb51bef3049c276e4
Sanu <sanukrishnansb@gmail.com>
2025-06-19 12:23:09 +0530

    Product hunt badge added

---
+++ src/components/ProductHuntBadge.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <a href="https://www.producthunt.com/products/testmagic?embed=true&utm_source=badge-featured&utm_medium=badge&utm_source=badge-testmagic" target="_blank">
+        <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=980631&theme=light&t=1750315594764" alt="TestMagic - AI&#0045;powered&#0032;API&#0032;test&#0032;automation&#0046;&#0032;Save&#0032;90&#0037;&#0032;of&#0032;testing&#0032;time&#0046; | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" />
+    </a>
+</template>
